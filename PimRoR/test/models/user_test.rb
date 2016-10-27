3c60af15e15879fd8555fc6622ede0dce3df6161
robnoflop <info@robertkasseck.de>
2016-10-27 16:50:10 +0200

    update user model

--- PimRoR/test/models/user_test.rb
+++ PimRoR/test/models/user_test.rb
@@ -6,7 +6,8 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(name: "Robert Kasseck", email: "s0543583@htw-berlin.de")
+    @user = User.new(name: "Robert Kasseck", email: "info@htw-berlin.de",
+                    password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -46,5 +47,22 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
