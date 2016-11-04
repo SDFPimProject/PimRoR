7be00269679d7389c5218822b5d5aa905013d223
robnoflop <info@robertkasseck.de>
2016-11-04 18:51:02 +0100

    move session controller to users scope

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+
+   def new 
+     @resource = User.new
+   end
 
    def edit
-
+     
    end
 
    def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'users/registrations', :sessions => "custom_sessions" }
+  devise_for :users, controllers: { registrations: 'users/registrations', :sessions => "users/sessions" }
   resources :projects
   resources :teams
   root 'welcome#index'
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get 'users/edit' => 'users/registrations#edit', :as => 'edit_user_registration'
     put 'users/edit' => 'users/registrations#update', :as => 'update_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    get 'users/create' => 'users/registrations#new' , :as => 'new_user_registration'
   end
 
 end
