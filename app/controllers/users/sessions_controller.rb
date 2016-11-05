fd538d6ae33a48fa3b776382cfe6804e852074f5
robnoflop <info@robertkasseck.de>
2016-11-05 19:22:41 +0100

    story1 (#7)
    move session controller to users scope
    add cancan ability model
    add user controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Users::SessionsController < Devise::SessionsController
+  after_filter :after_login, :only => :create
+  after_filter :after_logout, :only => :destroy
+
+  def after_login
+    puts('LOGIN')
+    user = User.find(current_user.id)
+    user.connection_id="LOGIN"
+    user.save!
+  end
+
+  def after_logout
+    puts('LOGOUT');
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-         #:registerable
+  #:registerable
 
   ROLES = %i[admin moderator author banned]
 
