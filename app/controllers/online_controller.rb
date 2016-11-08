01d78396b3074dabff8775364bf84145e156677c
ThunderAnimal <MartinWeber.93@outlook.com>
2016-11-08 18:34:08 +0100

    issue-15 solve login failed with validation

--- app/controllers/online_controller.rb
+++ app/controllers/online_controller.rb
@@ -4,20 +4,18 @@ class OnlineController < WebsocketRails::BaseController
   def new_connection
     user = User.find(current_user.id)
     #Prüfung ob neuer Login / bbzw. die Seite geschlossen und wieder geöffnet
-    if (user.connection_id == 'LOGIN') || (user.connection_id == '' && (Time.now - user.updated_at) > 3)
+    if user.connection_id == '' && (Time.now - user.updated_at) > 3
       #Wenn Ja Notification zum Client
       notification_to_user(user.id,{
-          :message => "Hey, " + user.first_name + "!\nWillkommen bei PIMSuite.",
-          :type => 'info'
+          :message => "Hey, " + user.first_name + "!\nWillkommen bei PIMSuite+.",
+          :type => 'success'
       })
     end
-    user.connection_id=client_id
-    user.save!
+    user.update_attribute(:connection_id, client_id)
   end
 
   def close_connection
     user = User.find(current_user.id)
-    user.connection_id=''
-    user.save!
+    user.update_attribute(:connection_id, '')
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,13 +3,9 @@ class Users::SessionsController < Devise::SessionsController
   after_filter :after_logout, :only => :destroy
 
   def after_login
-    puts('LOGIN')
-    user = User.find(current_user.id)
-    user.connection_id="LOGIN"
-    user.save!
+    flash[:success] = "Hey, " + current_user.first_name + "! Willkommen bei PIMSuite+."
   end
 
   def after_logout
-    puts('LOGOUT');
   end
 end
