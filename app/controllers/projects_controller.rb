ef426f03c0cfaa4d585ffb59cfaecf6fed232a05
robnoflop <info@robertkasseck.de>
2016-11-12 21:38:11 +0100

    add pagination to edit team and project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,7 +19,8 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1/edit
   def edit
-    @users = User.where.not(id: @project.users)
+    @users = User.where.not(id: @project.users).page(params[:userPage])
+    @project_users = @project.users.page(params[:projectUserPage])
   end
 
   # POST /projects/1/user/1/remove
