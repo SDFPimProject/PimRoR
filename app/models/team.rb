710cb5931521f8a50d1d3d23a1ade4b2e2bfb123
robnoflop <info@robertkasseck.de>
2016-11-20 23:31:50 +0100

    improve error rendering

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ActiveRecord::Base
     has_many :users_teams
     has_many :users, through: :users_teams
 
-    validates :name, length: { minimum: 2, maximum: 50 }, presence: true
+    validates :name, length: { minimum: 2, maximum: 50 }, presence: {message: "unzulässiges format"}
 
 end
