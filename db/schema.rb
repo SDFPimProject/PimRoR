0625aa1ac9bfe5c54fa55429e101f6f7a45a8492
robnoflop <info@robertkasseck.de>
2016-11-21 20:18:57 +0100

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115070457) do
+ActiveRecord::Schema.define(version: 20161121164735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20161115070457) do
     t.string   "state"
     t.integer  "zip_code"
     t.date     "birthday"
+    t.string   "locality"
+    t.string   "country_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
