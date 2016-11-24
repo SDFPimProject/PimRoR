9e9027a6628592a51f40e21744c39cd8206b4e0c
ThunderAnimal <MartinWeber.93@outlook.com>
2016-11-24 23:56:08 +0100

    fixed calender show bug

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,9 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
+document.addEventListener("turbolinks:load", function() {
     $('#calendar').fullCalendar(
         {events: '/events.json'}
     );
-
     $('#calendar_published_on').datepicker();
 });
