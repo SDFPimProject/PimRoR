4296f5bc864abf1187745e9a313e3a51dee1b721
robnoflop <info@robertkasseck.de>
2016-12-08 21:51:04 +0100

    fix Teams and Projects button visibility!

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,23 +1,23 @@
-class Ability
-  include CanCan::Ability
+  class Ability
+    include CanCan::Ability
 
-  def initialize(user)
-    if !user.nil? and user.role == "admin" || user.role == "manager"  
-      can :manage, :all
-    end
+    def initialize(user)
+      if !user.nil? and user.role == "admin" || user.role == "manager"  
+        can :manage, :all
+      end
 
-    if !user.nil? and user.users_teams.any? { |p| p.user_team_role == "manager"}
-      can :read, Team
-      can :update, Team
-    else
-      can :read, Team
-    end
+      if !user.nil? and user.users_teams.any? { |p| p.user_team_role == "manager"}
+        can :read, Team
+        can :update, Team
+      else
+        can :read, Team
+      end
 
-    if !user.nil? and user.users_projects.any? { |p| p.user_project_role == "manager"} 
-      can :read, Project
-      can :update, Project
-    else
-      can :read, Project
+      if !user.nil? and user.users_projects.any? { |p| p.user_project_role == "manager"} 
+        can :read, Project
+        can :update, Project
+      else
+        can :read, Project
+      end
     end
   end
-end
