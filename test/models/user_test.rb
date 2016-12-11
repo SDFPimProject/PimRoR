0b6b8a6b9d95c5f928c9599cbb92401f678503bc
robnoflop <info@robertkasseck.de>
2016-12-11 13:30:47 +0100

    add user yml

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
 
   test "should not save user without data" do
     user = User.new
     assert_not user.save
   end
+
+  test "should save user" do
+    user = users(:test)
+    assert user.save
+  end
+
+
+  
 end
