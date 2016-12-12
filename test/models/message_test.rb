c3919c07d0660bbe38ae786cff26b113ea0431fc
robnoflop <info@robertkasseck.de>
2016-12-12 16:12:23 +0100

    remove helper code

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,17 +1,5 @@
 require 'test_helper'
 
-
-    # conversation_id: 1
-    # send_from_id: 1
-    # is_receive: false
-    # is_read: false
-    # created_at: <%= Time.now %>
-    # updated_at: <%= Time.now %>
-    # is_send: false
-    # encrypted_body: "asdf"
-    # encrypted_body_iv: "asdf"
-
-
 class MessageTest < ActiveSupport::TestCase
 
   def setup
@@ -19,8 +7,8 @@ class MessageTest < ActiveSupport::TestCase
   end
 
   test "should not save message without data" do
-    m = Message.new
-    assert_not m.save
+      m = Message.new
+      assert_not m.save
   end
 
   test "should not valid read bevore send" do
