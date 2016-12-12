1ff26e4ec31b5d15d66b1d7e4e276ef41c094de9
robnoflop <info@robertkasseck.de>
2016-12-12 16:22:02 +0100

    add event test setup

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @event = events(:test)
+  end
+
+  test "should not save user without data" do
+    event = Event.new
+    assert_not event^^.save
+  end
 end
