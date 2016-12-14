677df9ac56eeeaf2346b2198c3f0a2167da72952
robnoflop <info@robertkasseck.de>
2016-12-14 22:13:39 +0100

    add conversations tests

--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ConversationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @conversation = conversations(:test)
+  end
+
+  test "should not save conversation without data" do
+      c = Conversation.new
+      assert_not c.save
+  end
+
+  test "should not valid created_at nil" do
+      @conversation.created_at = nil
+      assert_not @conversation.valid?
+  end
+
+  test "should not valid updated_at older than created_at" do
+      @conversation.updated_at = Time.now - 500
+      assert_not @conversation.valid?
+  end
+
+  test "should not valid sender_id lower than zero" do
+      @conversation.sender_id = -1
+      assert_not @conversation.valid?
+  end
+
+  test "should not valid recipient_id lower than zero" do
+      @conversation.recipient_id = -1
+      assert_not @conversation.valid?
+  end
 end
