7f6807e57e03ef556cb33741cbfd282a4cb18754
robnoflop <info@robertkasseck.de>
2016-12-18 16:14:12 +0100

    adds project tests (#91)

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   def setup
+      @project = projects(:test)
+    end
+
+    test "should not save project without data" do
+        p = Project.new
+        assert_not p.save
+    end
+
+    test "should not valid created_at nil" do
+      @project.created_at = nil
+      assert_not @project.valid?
+    end
+
+    test "should not valid updated_at older than created_at" do
+      @project.updated_at = Time.now - 500
+      assert_not @project.valid?
+    end
+
+    test "should not valid name to long" do
+      @project.name = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+      assert_not @project.valid?
+    end
+
+    test "should not valid name to short" do
+      @project.name = "abcd"
+      assert_not @project.valid?
+    end
+
+    test "should valid name" do
+      @project.name = "abcde"
+      assert @project.valid?
+    end
 end
