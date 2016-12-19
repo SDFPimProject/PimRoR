bf8f31c75bdfede45f0fc7632fc003165bf523cb
ThunderAnimal <MartinWeber.93@outlook.com>
2016-12-19 17:21:33 +0100

    fixed message validator

--- app/validators/message_validator.rb
+++ app/validators/message_validator.rb
@@ -1,13 +1,13 @@
 class MessageValidator < ActiveModel::Validator
     def validate(record)
 
-        unless (!record.is_send && record.is_receive )
+        if !record.is_send && record.is_receive
             record.errors[:is_receive] << 'is_receive could not be true before is_send'
         end
-        unless (!record.is_receive && record.is_read )
+        if !record.is_receive && record.is_read
             record.errors[:is_read] << 'is_read could not be true before is_receive'
         end
-        unless (record.encrypted_body.length == 0)
+        if record.encrypted_body.length == 0
             record.errors[:encrypted_body] << 'encrypted_body can not be empty'
         end
     end
