4c888c9068a2795c151b83cf197f5fbf4871ae82
robnoflop <info@robertkasseck.de>
2017-01-07 09:57:12 +0100

    update validation

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,5 +1,13 @@
 class Invite < ActiveRecord::Base
 
+
+validates :sender_comment, length: { maximum: 255 }, :allow_blank => true, presence: {message: "unzulässiges format"}
+validates :recipient_comment, length: { maximum: 255 }, :allow_blank => true, presence: {message: "unzulässiges format"}
+validates :event_id, :presence => true
+validates :sender_id, :presence => true
+validates :recipient_id, :presence => true
+
+
     scope :by_recipient_id_and_event_id, -> (recipient_id, event_id) do
       where("recipient_id = ? AND event_id = ?", recipient_id, event_id)
     end
