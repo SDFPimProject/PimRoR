9a6a414cb246b8ba7761df308fda8c5be8bf3b45
AijanaSchumann <aijana.schumann@gmail.com>
2017-01-08 16:20:56 +0100

    subscribed calendars viewable

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,15 @@ class Event < ActiveRecord::Base
       user_id_string = "user_id = " + user_id.to_s
       where("creator = ?", user_id_string)
     end
+
+    def as_json(options = {})
+    {
+      :title => self.title,
+      :start => self.start_time,
+      :end => self.end_time,
+      :description => self.description
+     }
+     end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-
   resources :users_projects
   resources :users_teams
   resources :events
   resources :projects
   resources :teams
   resources :subscribe_calendar
+  resources :test
   devise_for :users, controllers: {registrations: 'users/registrations', :sessions => "users/sessions"}
   root 'welcome#index'
 
@@ -40,8 +40,8 @@ Rails.application.routes.draw do
   post 'projects/:projectid/user/:userid/remove' => 'projects#removeUser', :as => 'project_remove_user'
   post 'projects/:projectid/user/:userid/add' => 'projects#addUser', :as => 'project_add_user'
 
-  get 'calendar_subscribe/:id/subscribe' =>'subscribe_calendar#subscribe', :as => 'subscribe_to_calendar_path'
-  get 'calendar_subscribe/:id/delete' =>'subscribe_calendar#delete', :as => 'delete_subscribtion_path'
+  get 'calendar/:id/subscribe' =>'subscribe_calendar#subscribe', :as => 'subscribe_to_calendar_path'
+  get 'calendar/:id/delete' =>'subscribe_calendar#delete', :as => 'delete_subscribtion_path'
+  get 'calendar/subscribed/:userid/' =>'subscribe_calendar#show', :as => 'subscribed_events_path'
 
-  get 'events/:id/subscribed' =>'events#subscribed', :as => 'subscribed_events_path'
 end
