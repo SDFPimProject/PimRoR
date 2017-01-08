26340a58f6dfa6c0a7ae880ce33460492de383cc
AijanaSchumann <aijana.schumann@gmail.com>
2017-01-08 16:20:56 +0100

    subscribed calendars viewable

--- app/controllers/subscribe_calendar_controller.rb
+++ app/controllers/subscribe_calendar_controller.rb
@@ -1,13 +1,20 @@
 class SubscribeCalendarController < ApplicationController
-layout "settingsmenu"
+ layout "settingsmenu", :only => [:index]
 
   def index
-  @users=User.all.page(params[:page])
+  require 'will_paginate/array'
+  @subs=current_user.subscribtions.all.page(params[:page])
+  
+  @users=User.all.reject { |user| @subs.exists?(calendar_id: user.id) }
+  @users.delete(current_user)
+  @users= @users.paginate(page: params[:page], per_page: 15)
+    
   end
 
   def subscribe
    @selectedUser = User.find(params[:id])
    @user=current_user
+
    if(!@user.subscribtions.exists?(calendar_id: @selectedUser.id))
        @sub= @user.subscribtions.create(calendar_id: @selectedUser.id, name:@selectedUser.first_name + ' '+ @selectedUser.last_name)
 
@@ -21,6 +28,12 @@ layout "settingsmenu"
   end
 
  def show
+  @events = Event.by_user_id(params[:userid])
+  @user= User.find_by_id(params[:userid])
+      respond_to do |format| 
+      format.html
+      format.json { render :json => @events.as_json } 
+    end
  end
 
  def delete
--- app/models/event.rb
+++ app/models/event.rb
@@ -13,4 +13,15 @@ class Event < ActiveRecord::Base
     #    where("(creator_id = ? OR id IN(SELECT event_id FROM invites WHERE recipient_id = ? AND recipient_status = 1))
     #            AND deleted = 0", user_id, user_id)
     #end
+
+    def as_json(options = {})
+    {
+      :title => self.title,
+      :start => self.start_time,
+      :end => self.end_time,
+      :description => self.description
+     }
+     end
+
+
 end
