9f2b70a2ea83f3d3290f9b83ffcd9068473bff13
robnoflop <info@robertkasseck.de>
2017-01-09 20:18:07 +0100

    format code

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,32 +1,27 @@
 class Event < ActiveRecord::Base
+  validates :start_time, date: { after_or_equal_to: Date.today , message: ' kann nicht in der Vergangenheit liegen.' }
+  validates :end_time, date: { after_or_equal_to: :start_time, message: ' muss nach start time liegen.' }
 
+  scope :by_user_id, -> (user_id) do
+    where("creator_id = ?", user_id)
+  end
 
+  scope :by_user_id_no_deleted, -> (user_id) do
+      where("(creator_id = ? OR id IN(SELECT event_id FROM invites WHERE recipient_id = ? AND recipient_status = 1))
+              AND deleted = 0", user_id, user_id)
+  end
 
-    validates :start_time, date: { after_or_equal_to: Date.today , message: ' kann nicht in der Vergangenheit liegen.' }
-    validates :end_time, date: { after_or_equal_to: :start_time, message: ' muss nach start time liegen.' }
+  #scope :by_user_id_no_deleted_and invites, -> (user_id) do
+  #    where("(creator_id = ? OR id IN(SELECT event_id FROM invites WHERE recipient_id = ? AND recipient_status = 1))
+  #            AND deleted = 0", user_id, user_id)
+  #end
 
-    scope :by_user_id, -> (user_id) do
-      where("creator_id = ?", user_id)
-    end
-
-    scope :by_user_id_no_deleted, -> (user_id) do
-        where("(creator_id = ? OR id IN(SELECT event_id FROM invites WHERE recipient_id = ? AND recipient_status = 1))
-                AND deleted = 0", user_id, user_id)
-    end
-
-    #scope :by_user_id_no_deleted_and invites, -> (user_id) do
-    #    where("(creator_id = ? OR id IN(SELECT event_id FROM invites WHERE recipient_id = ? AND recipient_status = 1))
-    #            AND deleted = 0", user_id, user_id)
-    #end
-
-    def as_json(options = {})
-    {
-      :title => self.title,
-      :start => self.start_time,
-      :end => self.end_time,
-      :description => self.description
-     }
-     end
-
-
+  def as_json(options = {})
+  {
+    :title => self.title,
+    :start => self.start_time,
+    :end => self.end_time,
+    :description => self.description
+  }
+  end
 end
